0db60bbf635ab3613605b8555df5e4237d519c5c
Caio Santos <caio.santos@msn.com>
2022-02-10 23:09:50 +0900

    add env

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -2,17 +2,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 const cryptoApiHeaders = {
   'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
-  'x-rapidapi-key': process.env.REACT_APP_CRYPTO_API_KEY,
+  'x-rapidapi-key': process.env.REACT_APP_API_RAPIDAPI_KEY,
 }
+
+const baseUrl = process.env.REACT_APP_CRYPTO_API_URL;
 
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
 export const cryptoApi = createApi({
   reducerPath: 'cryptoApi',
-  baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_CRYPTO_API_URL }),
+  baseQuery: fetchBaseQuery({ baseUrl }),
   endpoints: (builder) => ({
     getCryptos: builder.query({
-      query: (count) => createRequest(`/coins?limit=${count}`),
+      query: () => createRequest('/coins'),
     })
   }),
 });
