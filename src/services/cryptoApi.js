152c572fe3f53ce1cb4d54360442bd739eea94f0
Caio Santos <caio.santos@msn.com>
2022-02-10 18:57:38 +0900

    add store provider

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,19 @@
+
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     tiers: '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': '511371cb16mshea27035f1c65b7dp1f8988jsn41cf3c28dcf8'
+//   }
+// };
