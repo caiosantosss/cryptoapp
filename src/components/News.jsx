69380636f3755c3d1fc6189000e78cefc0bbdecd
Caio Santos <caio.santos@msn.com>
2022-02-13 17:47:48 +0900

    add crypto option

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,6 +3,7 @@ import { Select, Typography, Row, Col, Avatar, Card } from 'antd';
 import moment from 'moment';
 
 import { useGetCryptoNewsQuery } from '../services/cryptoNewsApi';
+import { useGetCryptosQuery } from '../services/cryptoApi';
 
 const { Text, Title } = Typography;
 const { Option } = Select;
@@ -11,7 +12,8 @@ const demoImage = 'https://picsum.photos/200';
 
 const News = ({ simplified }) => {
   const [newsCategory, setNewsCategory] = useState('Cryptocurrency');
-  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory: 'Cryptocurrency', count: simplified ? 6 : 12 });
+  const { data: cryptoNews } = useGetCryptoNewsQuery({ newsCategory, count: simplified ? 6 : 12 });
+  const { data } = useGetCryptosQuery(100);
 
   if (!cryptoNews?.value) return 'Loading...';
 
@@ -24,9 +26,11 @@ const News = ({ simplified }) => {
             className='select-news'
             placeholder='Select a Crypto'
             optionFilterProp='children'
-            onChange={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            onChange={(value) => setNewsCategory(value)}
+            filterOpiton={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
           >
             <Option value='Cryptocurrency'>Cryptocurrency</Option>
+            {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
           </Select>
         </Col>
       )}
