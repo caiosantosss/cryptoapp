87430c7c6a2242175de0bd3d651f03c9589eff7f
Caio Santos <caio.santos@msn.com>
2022-02-13 19:34:19 +0900

    add search for cryptonews

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -27,7 +27,7 @@ const News = ({ simplified }) => {
             placeholder='Select a Crypto'
             optionFilterProp='children'
             onChange={(value) => setNewsCategory(value)}
-            filterOpiton={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
+            filterOption={(input, option) => option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0}
           >
             <Option value='Cryptocurrency'>Cryptocurrency</Option>
             {data?.data?.coins.map((coin) => <Option value={coin.name}>{coin.name}</Option>)}
