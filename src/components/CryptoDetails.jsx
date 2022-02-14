00260f9de47f66abcf61c4f5c632fd5666d8141f
Caio Santos <caio.santos@msn.com>
2022-02-14 18:48:21 +0900

    add Title and option variables

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -5,6 +5,8 @@ import millify from 'millify'
 import { Col, Row, Typography, Select } from 'antd'
 import { MoneyCollectOutlined, DollarCircleOutlined, FundOutlined, ExclamationCircleOutlined, StopOutlined, TrophyOutlined, CheckOutlined, NumberOutlined, ThunderboltOutlined } from '@ant-design/icons';
 
+const { Title, Text } = Typography;
+const { Option } = Select;
 
 const CryptoDetails = () => {
   return (
