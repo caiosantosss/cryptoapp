73634145543a8b17b607580b7936d4a59f54e79d
Caio Santos <caio.santos@msn.com>
2022-02-15 21:55:41 +0900

    fix 24hVolume API

--- src/components/CryptoDetails.jsx
+++ src/components/CryptoDetails.jsx
@@ -18,6 +18,8 @@ const CryptoDetails = () => {
   const { data, isFetching } = useGetCryptoDetailsQuery(coinId);
   const cryptoDetails = data?.data?.coin;
 
+  // cryptoDetails["24hVolume"] = cryptoDetails.delete_24hVolume;
+
   // if (isFetching) return <Loader />;
 
   const time = ['3h', '24h', '7d', '30d', '1y', '3m', '3y', '5y'];
@@ -25,7 +27,7 @@ const CryptoDetails = () => {
   const stats = [
     { title: 'Price to USD', value: `$ ${cryptoDetails?.price && millify(cryptoDetails?.price)}`, icon: <DollarCircleOutlined /> },
     { title: 'Rank', value: cryptoDetails?.rank, icon: <NumberOutlined /> },
-    { title: '24h Volume', value: `$ ${cryptoDetails?.volume && millify(cryptoDetails?.volume)}`, icon: <ThunderboltOutlined /> },
+    { title: '24h Volume', value: `$ ${cryptoDetails?.["24hVolume"] && millify(Number(cryptoDetails?.["24hVolume"]))}`, icon: <ThunderboltOutlined /> },
     { title: 'Market Cap', value: `$ ${cryptoDetails?.marketCap && millify(cryptoDetails?.marketCap)}`, icon: <DollarCircleOutlined /> },
     { title: 'All-time-high(daily avg.)', value: `$ ${cryptoDetails?.allTimeHigh?.price && millify(cryptoDetails?.allTimeHigh?.price)}`, icon: <TrophyOutlined /> },
   ];
@@ -44,8 +46,42 @@ const CryptoDetails = () => {
     <Col className='coin-detail-container'>
       <Col className='coin-heading-container'>
         <Title level={2} className='coin-name'>
-          {data?.data?.coin.name} ({data?.data?.coin.symbol}) Price
+          {cryptoDetails?.name} ({cryptoDetails?.symbol}) Price
         </Title>
+        <p>
+          {cryptoDetails?.name} live price in US dollars.
+          View values statistics, market cap and supply details.
+        </p>
+      </Col>
+      <Select defaultValue='7d'
+        className='select-timeperiod'
+        placeholder='Select Time Period'
+        onChange={(value) => setTimePeriod(value)}
+      >
+        {time.map((date) => <Option key={date}>{date}</Option>)}
+
+      </Select>
+      {/* line chart.... */}
+      <Col className='stats-container'>
+        <Col className='coin-value-statistics'>
+          <Col className='coin-value-statistics-heading'>
+            <Title level={3} className='coin-details-heading'>
+              {cryptoDetails?.name} Value Statistics
+            </Title>
+            <p>
+              An overview showing the stats of {cryptoDetails?.name}
+            </p>
+          </Col>
+          {stats.map(({ icon, title, value }) => (
+            <Col className='coin-stats'>
+              <Col className='coin-stats-name'>
+                <Text>{icon}</Text>
+                <Text>{title}</Text>
+              </Col>
+              <Text className='stats'>{value}</Text>
+            </Col>
+          ))};
+        </Col>
       </Col>
     </Col>
   )
