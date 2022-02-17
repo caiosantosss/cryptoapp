e7a6e478eec84c543a91f6d8d5a326c207468768
Caio Santos <caio.santos@msn.com>
2022-02-17 15:10:58 +0900

    fix the Linechart do left to the right

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -1,70 +1,75 @@
 import React from 'react';
 import { Line } from 'react-chartjs-2';
 import { Col, Row, Typography } from 'antd';
-import { Chart as ChartJS } from 'chart.js/auto';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js'
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+)
 
-const { Title } = Typography;
+const { Title: AntTitle } = Typography;
 
 const LineChart = ({ coinHistory, currentPrice, coinName }) => {
   const coinPrice = [];
   const coinTimestamp = [];
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinPrice.push(coinHistory?.data?.history[i]?.price);
+    coinPrice.unshift(coinHistory?.data?.history[i]?.price);
   }
 
   for (let i = 0; i < coinHistory?.data?.history?.length; i += 1) {
-    coinTimestamp.push(new Date(coinHistory?.data?.history[i]?.timestamp * 1000).toLocaleDateString());
+    coinTimestamp.unshift(new Date(coinHistory?.data?.history[i]?.timestamp * 1000).toLocaleDateString());
   }
 
-  console.log(coinTimestamp);
-
-  const config = {
+  const data = {
     labels: coinTimestamp,
     datasets: [
       {
-        label: 'Price in USD',
+        label: 'Price In USD',
         data: coinPrice,
         fill: false,
         backgroundColor: '#0071bd',
         borderColor: '#0071bd',
-      }
+      },
     ],
-    options: {
-      scales: {
-        yAxes: [{
+  };
+  const options = {
+    scales: {
+      yAxes: [
+        {
           ticks: {
-            reverse: true,
-          }
-        }]
-      }
-    }
+            beginAtZero: true,
+          },
+        },
+      ],
+    },
   };
 
-
-  // const options = {
-  //   scales: {
-  //     yAxes: [
-  //       {
-  //         ticks: {
-  //           beginAtZero: true,
-  //           reverse: true,
-  //         },
-  //       },
-  //     ],
-  //   },
-  // };
 
   return (
     <>
       <Row className='chart-header'>
-        <Title level={2} className='chart-title'>{coinName} Price History</Title>
+        <AntTitle level={2} className='chart-title'>{coinName} Price History</AntTitle>
         <Col className='price-container'>
-          <Title level={5} className='price-change'>Change: {coinHistory?.data?.change}%</Title>
-          <Title level={5} className='current-price'>Current {coinName} Price: $ {currentPrice}</Title>
+          <AntTitle level={5} className='price-change'>Change: {coinHistory?.data?.change}%</AntTitle>
+          <AntTitle level={5} className='current-price'>Current {coinName} Price: $ {currentPrice}</AntTitle>
         </Col>
       </Row>
-      <Line data={config} />
+      <Line data={data} options={options} />
     </>
   )
 }
